ef3df2b4f1e569c513dd3a0afad7de3eae33f213
Qusai Al Shidi <me@qalshidi.science>
2020-09-26 20:00:36 -0400

    Feature: <C-z> for fg in fish stays for fish 3.2

--- .config/fish/conf.d/bindings.fish
+++ .config/fish/conf.d/bindings.fish
@@ -1,1 +1,6 @@
-bind \ez 'fg'        # <A-z> brings process back to foreground
+# For fish 3.2.0 update
+function fish_user_key_bindings
+   bind \cz 'fg 2>/dev/null; commandline -f repaint'
+end
+# <A-z> brings process back to foreground
+bind \ez 'fg 2>/dev/null; commandline -f repaint'
