6295b3f5eb0007f779558113a7d525923086d3f6
Qusai Al Shidi <me@qalshidi.science>
2020-10-06 15:20:40 -0400

    Add ability for emojis

--- .config/fish/conf.d/bindings.fish
+++ .config/fish/conf.d/bindings.fish
@@ -3,17 +3,14 @@ bind \cz 'fg 2>/dev/null; commandline -f repaint'
 # <A-z> brings process back to foreground
 bind \ez 'fg 2>/dev/null; commandline -f repaint'
 
-# skim
-[ -z $SMIK_TMUX ] && type -q sk-tmux && set skimcmd sk-tmux || set skimcmd sk
-
-if type -q sk
-    bind \eg "skim_grep"
-    bind \ct "commandline -it \'($skimcmd)\'"
-end
-
 function skim_grep
     set result ($skimcmd --ansi -i -c 'rg --color=always --vimgrep {}' \
         | sed 's/"/\\\\"/g')
     sleep 0.001
     nvim +cexpr!" \"$result\"" -
+end
+
+if type -q sk
+    bind \eg "skim_grep"
+    bind \ct "commandline -i \'($skimcmd)\'"
 end
