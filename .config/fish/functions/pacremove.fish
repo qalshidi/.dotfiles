efd0c326ef583bffab5a8d6339597288bcdcf903
Qusai Al Shidi <me@qalshidi.science>
2021-02-02 21:35:39 -0500

    Tue Feb  2 09:35:39 PM EST 2021

--- .config/fish/functions/pacremove.fish
+++ .config/fish/functions/pacremove.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function pacremove --wraps='sudo powerpill -Rns' --description 'alias pacremove=sudo powerpill -Rns'
-  sudo powerpill -Rns $argv;
+  nice --adjustment=10 sudo powerpill -Rns $argv;
 end
--- .config/fish/functions/pacupgrade.fish
+++ .config/fish/functions/pacupgrade.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function pacupgrade --wraps='yay -Syu' --description 'alias pacupgrade=yay -Syu'
-  yay --pacman powerpill -Syu $argv;
+  nice --adjustment=10 yay --pacman powerpill -Syu $argv;
 end
