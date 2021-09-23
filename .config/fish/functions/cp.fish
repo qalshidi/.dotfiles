7123c7334e188fa19b57483c13c59d9017e729a9
Qusai Al Shidi <me@qalshidi.science>
2021-09-23 13:17:41 -0400

    add time

--- .config/fish/functions/cp.fish
+++ .config/fish/functions/cp.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function cp --wraps='rsync -avP' --description 'alias cp=rsync -avP'
-  rsync -avP $argv;
+function cp --wraps='rsync -atvP' --description 'alias cp=rsync -atvP'
+  rsync -atvP $argv;
 end
